30b98d9b46b1c1cf0f805eff4416c1ef47a25bde
arcolinuxz <arcolinuxinfo@gmail.com>
2022-01-08 08:01:17 +0100

    textext

--- alis-recovery.sh
+++ alis-recovery.sh
@@ -401,6 +401,7 @@ function main() {
 
 main
 echo
-echo "Once finalized recovery tasks execute following commands:"
 echo "Your system has been mounted in /mnt."
+echo
+echo "Once finalized recovery tasks execute following commands:"
 echo "umount -R /mnt and reboot."
