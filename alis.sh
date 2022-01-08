5343607bcb640609aa0d56523e6985845965c6b6
Erik Dubois <erik.dubois@gmail.com>
2022-01-08 16:54:59 +0100

    xorgserver

--- alis.sh
+++ alis.sh
@@ -204,7 +204,7 @@ function check_variables() {
     check_variables_value "HOOKS" "$HOOKS"
     check_variables_list "BOOTLOADER" "$BOOTLOADER" "grub refind systemd" "true" "true"
     check_variables_list "CUSTOM_SHELL" "$CUSTOM_SHELL" "bash zsh dash fish" "true" "true"
-    check_variables_list "DESKTOP_ENVIRONMENT" "$DESKTOP_ENVIRONMENT" "gnome kde xfce mate cinnamon lxde i3-wm i3-gaps deepin budgie bspwm" "false" "true"
+    check_variables_list "DESKTOP_ENVIRONMENT" "$DESKTOP_ENVIRONMENT" "gnome kde xfce mate cinnamon lxde i3-wm i3-gaps deepin budgie bspwm awesome qtile leftwm" "false" "true"
     check_variables_boolean "PACKAGES_MULTILIB" "$PACKAGES_MULTILIB"
     check_variables_boolean "PACKAGES_INSTALL" "$PACKAGES_INSTALL"
     check_variables_boolean "VAGRANT" "$VAGRANT"
@@ -1659,6 +1659,15 @@ function desktop_environment() {
         "bspwm" )
             desktop_environment_bspwm
             ;;
+        "awesome" )
+            desktop_environment_awesome
+            ;;
+        "qtile" )
+            desktop_environment_qtile
+            ;;
+        "leftwm" )
+            desktop_environment_leftwm
+            ;;
     esac
 
     arch-chroot /mnt systemctl set-default graphical.target
@@ -1716,7 +1725,24 @@ function desktop_environment_budgie() {
 }
 
 function desktop_environment_bspwm() {
-    pacman_install "bspwm lightdm lightdm-gtk-greeter xorg-server"
+    pacman_install "bspwm lightdm lightdm-gtk-greeter"
+    arch-chroot /mnt systemctl enable lightdm.service
+}
+
+function desktop_environment_awesome() {
+    pacman_install "awesome vicious xterm lightdm lightdm-gtk-greeter xorg-server"
+    arch-chroot /mnt systemctl enable lightdm.service
+}
+
+function desktop_environment_qtile() {
+    pacman_install "qtile xterm lightdm lightdm-gtk-greeter xorg-server"
+    arch-chroot /mnt systemctl enable lightdm.service
+}
+
+function desktop_environment_leftwm() {
+    #leftwm-git in AUR - set in alis-packages.conf
+    #leftwm-theme-git in AUR - set in alis-packages.conf
+    pacman_install "dmenu xterm lightdm lightdm-gtk-greeter xorg-server"
     arch-chroot /mnt systemctl enable lightdm.service
 }
 
