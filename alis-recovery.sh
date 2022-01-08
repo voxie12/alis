06ad2bbb99cc68e9dedfa6d667600ef476b63bb5
arcolinuxz <arcolinuxinfo@gmail.com>
2022-01-08 08:13:05 +0100

    typo

--- alis-recovery.sh
+++ alis-recovery.sh
@@ -180,7 +180,7 @@ function warning() {
     echo "of the alis-recovery.conf file."
     echo
 	echo "You will need to arch-chroot into /mnt"
-	echo "arch-chroot /mnt
+	echo "arch-chroot /mnt"
 	echo
     echo "Once finalized recovery tasks execute following commands:"
     echo "exit, umount -R /mnt and reboot."
