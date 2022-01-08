8fd00ff1429724f3b793b7bd078ff5abc37decd1
arcolinuxz <arcolinuxinfo@gmail.com>
2022-01-08 08:12:21 +0100

    text

--- alis-recovery.sh
+++ alis-recovery.sh
@@ -176,11 +176,14 @@ function check_variables_size() {
 function warning() {
     echo -e "${LIGHT_BLUE}Welcome to Arch Linux Install Script Recovery${NC}"
     echo ""
-    echo "We will use arch-chroot and mount your system based on the settings"
+    echo "We will mount your system based on the settings"
     echo "of the alis-recovery.conf file."
     echo
+	echo "You will need to arch-chroot into /mnt"
+	echo "arch-chroot /mnt
+	echo
     echo "Once finalized recovery tasks execute following commands:"
-    echo "umount -R /mnt, reboot."
+    echo "exit, umount -R /mnt and reboot."
     echo ""
     read -p "Do you want to continue? [y/N] " yn
     case $yn in
